791dc85f300f6c16ceb175698b57fc003466ed8d
roshanxtranet <roshan@extranet.ae>
2021-06-25 18:19:47 +0530

    grid added

--- lib/ui/screens/leaves/widget/grid_widget.dart
+++ lib/ui/screens/leaves/widget/grid_widget.dart
@@ -1,44 +1,82 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-Widget buildFront(String templateName) {
-  return Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: ListTile(
-      title: Container(
-        decoration: BoxDecoration(
-            color: Colors.white, border: Border.all(color: Colors.black)),
-        child: Center(
-          child: Icon(
-            Icons.access_alarms_rounded,
-            size: 50,
+import 'package:flutter/material.dart';
+import 'package:hr_management/constants/api_endpoints.dart';
+
+final _random = Random();
+Widget buildFront(String templateName, String templateImageCode) {
+  return Card(
+    elevation: 5.0,
+    child: Container(
+      child: Column(
+        children: [
+          Container(
+            color: Colors.primaries[_random.nextInt(Colors.primaries.length)]
+                [_random.nextInt(9) * 100],
+            child: Container(
+              margin: EdgeInsets.all(8),
+              child: Center(
+                child: Image.network(APIEndpointConstants.BASE_URL +
+                    '/Document/GetImageMongo?id=' +
+                    templateImageCode),
+              ),
+            ),
           ),
-        ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            templateName,
+            textAlign: TextAlign.center,
+            maxLines: 1,
+          ),
+        ],
       ),
-      subtitle: Container(
-          height: 30,
-          decoration: BoxDecoration(
-              color: Colors.blue, border: Border.all(color: Colors.black)),
-          child: Center(child: Text(templateName))),
     ),
   );
 }
 
 Widget buildRear(String templateName) {
-  return Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: ListTile(
-      title: Container(
-        decoration: BoxDecoration(
-            color: Colors.white, border: Border.all(color: Colors.black)),
-        child: Center(
-          child: Text(templateName),
-        ),
-      ),
-      subtitle: Container(
-        height: 30,
-        child: ElevatedButton(
-          onPressed: () {},
-          child: Text('Create'),
+  Color cardBackground =
+      Colors.primaries[_random.nextInt(Colors.primaries.length)]
+          [_random.nextInt(7) * 100];
+  Color textColor = cardBackground != null
+      ? cardBackground.computeLuminance() > 0.5
+          ? Colors.black
+          : Colors.white
+      : Colors.black;
+  return Card(
+    elevation: 5.0,
+    child: Container(
+      color: cardBackground,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Container(
+                margin: EdgeInsets.all(8),
+                alignment: Alignment.center,
+                child: Text(
+                  templateName,
+                  style: TextStyle(color: textColor),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Create",
+                    textAlign: TextAlign.center,
+                    maxLines: 1,
+                  )),
+            ),
+          ],
         ),
       ),
     ),
