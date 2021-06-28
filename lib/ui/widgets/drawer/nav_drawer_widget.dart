b8f8a2b9da3c7c38dd6872695ba00c7e7f8d8059
Vani Shrivastava <vani@extranet.ae>
2021-06-28 10:56:41 +0530

    drawer menu changes

--- lib/ui/widgets/drawer/nav_drawer_widget.dart
+++ lib/ui/widgets/drawer/nav_drawer_widget.dart
@@ -74,16 +74,16 @@ Widget drawerWidget(context) {
             );
           },
         ),
-        ListTile(
-          title: Text('Create Service'),
-          trailing: Icon(Icons.create),
-          onTap: () {
-            Navigator.pushReplacementNamed(
-              context,
-              CREATE_SERVICE,
-            );
-          },
-        ),
+        // ListTile(
+        //   title: Text('Create Service'),
+        //   trailing: Icon(Icons.create),
+        //   onTap: () {
+        //     Navigator.pushReplacementNamed(
+        //       context,
+        //       CREATE_SERVICE,
+        //     );
+        //   },
+        // ),
       ],
     ),
   );
