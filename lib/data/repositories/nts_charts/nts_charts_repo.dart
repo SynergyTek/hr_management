b0dda4c94c98bd8d97ab43357117e9f9d4aa05d8
Vani Shrivastava <vani@extranet.ae>
2021-07-16 18:32:25 +0530

    repo function implementation

--- lib/data/repositories/nts_charts/nts_charts_repo.dart
+++ lib/data/repositories/nts_charts/nts_charts_repo.dart
@@ -38,13 +38,63 @@ class NTSChartRepository extends AbstractNTSChartRepository {
   }
 
   @override
-  Future<ChartResponse> getChartByUserType({Map<String, dynamic> queryparams, NTSType ntsType}) {
-    throw UnimplementedError();
+  Future<ChartResponse> getChartByUserType({Map<String, dynamic> queryparams, NTSType ntsType})  async {
+    String endpoint = '';
+    if (ntsType == NTSType.service) {
+      endpoint = APIEndpointConstants.GET_SERVICE_CHART_BY_USERTYPE;
+    } else if (ntsType == NTSType.note) {
+      endpoint = APIEndpointConstants.GET_NOTE_CHART_BY_USERTYPE;
+    } else if (ntsType == NTSType.task) {
+      endpoint = APIEndpointConstants.GET_TASK_CHART_BY_USERTYPE;
+    }
+
+    try {
+      Response response = await _dio.get(
+        endpoint,
+        queryParameters: queryparams ?? {},
+      );
+
+      print(response.data);
+      return ChartResponse.fromJson(
+        response.data,
+      );
+    } catch (err, stacktrace) {
+      print(
+          "[Exception]: Error occured while fetching the API Response for endpoint: $endpoint.");
+      print("Stacktrace: $stacktrace \nError: $err");
+
+      return ChartResponse.withError("$err");
+    }
   }
 
   @override
-  Future<ChartResponse> getDatewiseSLA({Map<String, dynamic> queryparams, NTSType ntsType}) {
-    throw UnimplementedError();
+  Future<ChartResponse> getDatewiseSLA({Map<String, dynamic> queryparams, NTSType ntsType})  async {
+    String endpoint = '';
+    if (ntsType == NTSType.service) {
+      endpoint = APIEndpointConstants.READ_DATEWISE_SERVICE_SLA;
+    // } else if (ntsType == NTSType.note) {
+    //   endpoint = APIEndpointConstants.GET_NOTE_CHART_BY_STATUS;
+    } else if (ntsType == NTSType.task) {
+      endpoint = APIEndpointConstants.READ_DATEWISE_TASK_SLA;
+    }
+
+    try {
+      Response response = await _dio.get(
+        endpoint,
+        queryParameters: queryparams ?? {},
+      );
+
+      print(response.data);
+      return ChartResponse.fromJson(
+        response.data,
+      );
+    } catch (err, stacktrace) {
+      print(
+          "[Exception]: Error occured while fetching the API Response for endpoint: $endpoint.");
+      print("Stacktrace: $stacktrace \nError: $err");
+
+      return ChartResponse.withError("$err");
+    }
   }
 
 
