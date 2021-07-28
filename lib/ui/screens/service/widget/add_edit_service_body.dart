b71cd64949c406d28f204cff0f9190485d2464a2
Vani Shrivastava <vani@extranet.ae>
2021-07-28 17:33:14 +0530

    fix

--- lib/ui/screens/service/widget/add_edit_service_body.dart
+++ lib/ui/screens/service/widget/add_edit_service_body.dart
@@ -86,19 +86,6 @@ class _CreateServiceScreenBodyState extends State<CreateServiceScreenBody> {
   }
 
   @override
-<<<<<<< HEAD
-=======
-  void initState() {
-    super.initState();
-    serviceBloc
-      ..getServiceDetail(
-        templateCode: widget.templateCode,
-        serviceId: widget.serviceId,
-      );
-  }
-
-  @override
->>>>>>> 0f1c2ca9d5141b3d03cc1d7425303b77dbb4982b
   Widget build(BuildContext context) {
     final createServiceFormBloc = context.read<CreateServiceFormBloc>();
 
@@ -1198,12 +1185,8 @@ class _CreateServiceScreenBodyState extends State<CreateServiceScreenBody> {
   String resultMsg = '';
   serviceViewModelPostRequest(int postDataAction, String serviceStatusCode,
       CreateServiceFormBloc createServiceFormBloc) async {
-<<<<<<< HEAD
-    String stringModel = jsonEncode(widget.serviceModel);
-=======
     String userId = await getUserId();
-    String stringModel = jsonEncode(serviceModel);
->>>>>>> 0f1c2ca9d5141b3d03cc1d7425303b77dbb4982b
+    String stringModel = jsonEncode(widget.serviceModel);
     var jsonModel = jsonDecode(stringModel);
     postServiceModel = Service.fromJson(jsonModel);
 
