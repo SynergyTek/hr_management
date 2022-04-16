8e0b611739f74d20651f1d9665b1ec45ed75f218
Inzemam <inzemam@extranet.ae>
2022-04-16 17:57:10 +0530

    business trip changes

--- lib/ui/screens/business_trip/widget/business_trip_widget.dart
+++ lib/ui/screens/business_trip/widget/business_trip_widget.dart
@@ -3,9 +3,12 @@ import 'package:flutter_form_bloc/flutter_form_bloc.dart';
 import 'package:hr_management/data/models/business_trip_model/business_trip_model.dart';
 import 'package:hr_management/data/models/business_trip_model/business_trip_response_model.dart';
 import 'package:hr_management/logic/blocs/leave_bloc.dart';
+import 'package:hr_management/routes/screen_arguments.dart';
 import 'package:hr_management/themes/theme_config.dart';
 import 'package:hr_management/ui/widgets/progress_indicator.dart';
 import '../../../../../logic/blocs/user_model_bloc/user_model_bloc.dart';
+import '../../../../routes/route_constants.dart';
+import '../../../widgets/widgets.dart';
 
 class BusinessTripBody extends StatefulWidget {
   BusinessTripBody({Key? key}) : super(key: key);
@@ -54,50 +57,61 @@ class _BusinessTripBodyState extends State<BusinessTripBody> {
               var list = snapshot.data.data;
               List<BusinessTripModel> model = list.toList();
 
-              return GridView.builder(
-                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    childAspectRatio: 2 / 2.2,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20),
-                itemCount: model.length,
-                itemBuilder: (context, index) {
-                  BusinessTripModel item = model[index];
-                  return GestureDetector(
-                    child: Card(
-                      margin: DEFAULT_PADDING,
-                      elevation: 6,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          (item.serviceNo != null)
-                              ? Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(left: 8.0),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Flexible(
-                                          child: Text(
-                                            item.serviceNo.toString(),
-                                            maxLines: 2,
-                                            textAlign: TextAlign.center,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                )
-                              : Container(),
-                        ],
-                      ),
-                    ),
-                    onTap: () {},
-                  );
-                },
-              );
+              return ListView.builder(
+                  itemCount: model.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            CREATE_SERVICE_ROUTE,
+                            arguments: ScreenArguments(
+                              arg1: "",
+                              arg2: model[index].id,
+                              arg3: "",
+                              val1: false,
+                            ),
+                          );
+                        },
+                        leading: Icon(Icons.business_center_rounded),
+                        trailing: Text(
+                          model[index].status ?? "-",
+                          style: TextStyle(color: Colors.green, fontSize: 15),
+                        ),
+                        title: Container(
+                          padding: DEFAULT_VERTICAL_PADDING,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Purpose - " +
+                                  (model[index].purpose ?? "NA")),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text("Start Date"),
+                                  Text(" - "),
+                                  Text("End Date"),
+                                ],
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(formatDate(
+                                          date: model[index]
+                                              .businessTripStartDate) ??
+                                      "NA"),
+                                  Text(" - "),
+                                  Text(formatDate(
+                                          date: model[index]
+                                              .businessTripEndDate) ??
+                                      "NA")
+                                ],
+                              ),
+                              Divider(color: Colors.black)
+                            ],
+                          ),
+                        ));
+                  });
             } else {
               return Center(
                 child: CustomProgressIndicator(),
