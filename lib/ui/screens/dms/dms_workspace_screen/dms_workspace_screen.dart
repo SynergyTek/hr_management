4214c5c00776a44ab62c93a71e5aff7591467375
VidhishaPB <vidhisha@extranet.ae>
2022-04-29 11:38:47 +0530

    Add new workspace

--- lib/ui/screens/dms/dms_workspace_screen/dms_workspace_screen.dart
+++ lib/ui/screens/dms/dms_workspace_screen/dms_workspace_screen.dart
@@ -4,6 +4,8 @@ import 'package:hr_management/ui/widgets/appbar_widget.dart';
 import 'package:hr_management/ui/widgets/drawer/nav_drawer_widget.dart';
 import 'package:hr_management/ui/widgets/internet_connectivity_widget.dart';
 
+import '../../../../routes/route_constants.dart';
+
 class DMSWorkspaceScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,6 +17,14 @@ class DMSWorkspaceScreen extends StatelessWidget {
       body: SafeArea(
         child: InternetConnectivityWidget(
           child: DMSWorkspaceBody(),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.pushNamed(
+          context,
+          DMS_MANAGE_WORKSPACE_ROUTE,
         ),
       ),
     );
