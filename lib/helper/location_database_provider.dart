bcb3c1d23e5580380aad067203086422cadefdda
DESKTOP-SSP9JLB\roshan <roshan@extranet.ae>
2023-03-04 14:18:35 +0530

    changes

--- lib/helper/location_database_provider.dart
+++ lib/helper/location_database_provider.dart
@@ -6,6 +6,83 @@ import 'package:sqflite/sqflite.dart';
 
 import '../data/models/user_location_model.dart';
 
+// class LocationDatabaseProvider {
+//   // Singleton pattern
+//   static final LocationDatabaseProvider _databaseService =
+//       LocationDatabaseProvider._internal();
+//   factory LocationDatabaseProvider() => _databaseService;
+//   LocationDatabaseProvider._internal();
+
+//   static Database? _database;
+
+//   Future<Database> get database async {
+//     if (_database != null) return _database!;
+//     // Initialize the DB first time it is accessed
+//     _database = await _initDatabase();
+//     return _database!;
+//   }
+
+//   Future<Database> _initDatabase() async {
+//     final databasePath = await getDatabasesPath();
+
+//     // Set the path to the database. Note: Using the `join` function from the
+//     // `path` package is best practice to ensure the path is correctly
+//     // constructed for each platform.
+//     final path = join(databasePath, 'hrm_location.db');
+
+//     // Set the version. This executes the onCreate function and provides a
+//     // path to perform database upgrades and downgrades.
+//     return await openDatabase(
+//       path,
+//       onCreate: _onCreate,
+//       version: 1,
+//       onConfigure: (db) async => await db.execute('PRAGMA foreign_keys = ON'),
+//     );
+//   }
+
+//   // When the database is first created, create a table to store breeds
+//   // and a table to store dogs.
+//   Future<void> _onCreate(Database db, int version) async {
+//     // Run the CREATE {locations} TABLE statement on the database.
+//     await db.execute(
+//       'CREATE TABLE locations(id INTEGER PRIMARY KEY, time TEXT, latitude REAL, longitude REAL)',
+//     );
+//   }
+
+//   Future<void> insertLocation(UserLocation userLocation) async {
+//     // Get a reference to the database.
+//     final db = await _databaseService.database;
+
+//     // Insert the Locations into the correct table. You might also specify the
+//     // `conflictAlgorithm` to use in case the same Location is inserted twice.
+//     //
+//     // In this case, replace any previous data.
+//     await db.insert(
+//       'locations',
+//       userLocation.toMap(),
+//       conflictAlgorithm: ConflictAlgorithm.replace,
+//     );
+//   }
+
+//   Future<List<UserLocation>> getAllUserLocations() async {
+//     // Get a reference to the database.
+//     final db = await _databaseService.database;
+
+//     // Query the table for all the locations.
+//     final List<Map<String, dynamic>> maps = await db.query('locations');
+
+//     try {
+//       // Convert the List<Map<String, dynamic> into a List<Breed>.
+//       List<UserLocation> list = List.generate(
+//           maps.length, (index) => UserLocation.fromMap(maps[index]));
+//       return list;
+//     } catch (e) {
+//       print(e);
+//       return [];
+//     }
+//   }
+// }
+
 class LocationDatabaseProvider {
 // Singleton pattern
   static final LocationDatabaseProvider _databaseService =
@@ -36,11 +113,11 @@ class LocationDatabaseProvider {
     });
   }
 
-  addLocationToDatabase(UserLocation alertLocation) async {
+  addLocationToDatabase(UserLocation userLocation) async {
     final db = await database;
     var raw = await db.insert(
       "UserLocation",
-      alertLocation.toJson(),
+      userLocation.toJson(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
     return raw;
